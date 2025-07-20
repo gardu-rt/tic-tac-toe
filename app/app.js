fc1f6f56810ae8bfe2fc84564fa6c099cf43c8cb
kapindo <kapindo@gmail.com>
2025-07-20 13:18:32 +0700

    refactor, add win and draw condition create game over condition

--- app/app.js
+++ app/app.js
@@ -1,65 +1,119 @@
-const Gameboard = function () {
-  const board = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-  ];
+const gameboard = function () {
+  const CELL = 9;
+  let board = ["", "", "", "", "", "", "", "", ""];
 
-  const getBoard = () => board;
-  const markBoard = (row, column, mark) => {
-    if (board[row][column]) {
-      return false;
-    };
-    board[row][column] = mark;
-  };
-
-  return { getBoard, markBoard };
-};
-
-function Gamecontrol() {
-  const board = Gameboard();
-
-  const coinSide = ["Tail", "Head"];
-
-  function getRandomCoin() {
-    let index = Math.floor(Math.random() * coinSide.length);
-    return coinSide[index];
+  function makeMove(player, cell) {
+    board[cell] = player;
   }
 
-  const coin = getRandomCoin();
-  console.log(coin);
+  function getBoard() {
+    return board;
+  }
 
-  let playerOne = coin === "Head" ? "X" : "O";
-  let playerTwo = coin === "Tail" ? "X" : "O";
-
-  let activePlayer = coin === "Head";
-
-  const getActivePlayer = () => {
-    return activePlayer ? playerOne : playerTwo;
-  };
-
-  const getTurnInfo = () => {
-    if (activePlayer) {
-      console.log("Player One Turn");
-    } else {
-      console.log("Player Two Turn");
+  function clearBoard() {
+    board = [];
+    for (let i = 0; i < CELL; i++) {
+      board.push("");
     }
-  };
+  }
 
-  getTurnInfo();
+  function printBoard() {
+    console.clear();
+    console.log(`
+    | ${board[0]} | ${board[1]} | ${board[2]} |
+    | ${board[3]} | ${board[4]} | ${board[5]} |
+    | ${board[6]} | ${board[7]} | ${board[8]} |
+    `);
+  }
 
-  const playerRound = function (row, column) {
-    if (board.getBoard()[row][column]) {
-      console.log("This cell is marked");
+  return { makeMove, printBoard, getBoard, clearBoard };
+};
+
+const gamecontrol = function () {
+  const board = gameboard();
+
+  let currentPlayer = "X";
+
+  function switchPlayer() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  }
+
+  function isValidMove(cell) {
+    return !board.getBoard()[cell] ? true : false;
+  }
+
+  function checkWinner() {
+    const winnerPattern = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],  // horizontal pattern
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical pattern
+      [0, 4, 8], [2, 4, 6] // diagonal pattern
+    ];
+
+    for (pattern of winnerPattern) {
+      const [a, b, c] = pattern;
+
+      if (board.getBoard()[a] !== ""
+        && board.getBoard()[a] === board.getBoard()[b]
+        && board.getBoard()[b] === board.getBoard()[c]) {
+
+        return true;
+
+      }
+    }
+
+    return false;
+  }
+
+  function checkDraw() {
+    for (cell of board.getBoard()) {
+
+      if (cell === "") return false;
+
+    }
+
+    return true;
+  }
+
+  let gameOver = false;
+
+  function gameResult() {
+    if (checkWinner()) {
+      console.log(`${currentPlayer} Win`);
+      gameOver = true;
+    }
+
+    else if (checkDraw()) {
+      console.log("no winner");
+      gameOver = true;
+    }
+  }
+
+  function resetGame() {
+    gameOver = false;
+
+    board.clearBoard();
+    board.printBoard();
+  }
+
+  function playRound(cell) {
+    if (gameOver) return;
+
+    if (!isValidMove(cell)) {
+      console.log("invalid move try again");
       return;
-    };
-    board.markBoard(row, column, getActivePlayer());
-    console.log(board.getBoard());
-    activePlayer = !activePlayer;
-    getTurnInfo();
-  };
+    }
 
-  return { playerRound };
-}
+    board.makeMove(currentPlayer, cell);
+    board.printBoard();
+    gameResult();
 
-const game = Gamecontrol();
+    if (gameOver) return;
+
+    switchPlayer();
+  }
+
+  return { playRound, resetGame };
+
+};
+
+const game = gamecontrol();
